741f7083179955307202e77fb77ee47bc9ec8da6
zach <zachbogard@gmail.com>
2020-05-01 20:08:39 -0400

    fix gitignore

---
+++ webpack.prod.config.js
@@ -0,0 +1,74 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const LinkTypePlugin = require('html-webpack-link-type-plugin')
+  .HtmlWebpackLinkTypePlugin;
+const CopyPlugin = require('copy-webpack-plugin');
+
+const { resolve } = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  node: {
+    fs: 'empty',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // only enable hot in development
+              hmr: process.env.NODE_ENV === 'development',
+              // if hmr does not work, this is a forceful method.
+              reloadAll: true,
+            },
+          },
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: resolve(__dirname, 'public', 'index.html'),
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new LinkTypePlugin({
+      '*.css': 'text/css',
+    }),
+    new CopyPlugin([{ from: 'public', to: 'public' }]),
+  ],
+};
